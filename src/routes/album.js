50bdb82e3eef697d8d86513e318d62d864bbf1d7
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-03-18 11:47:16 +0000

    fixed route issue

--- src/routes/album.js
+++ src/routes/album.js
@@ -5,7 +5,7 @@ const albumController = require('../controllers/album');
 
 const router = express.Router();
 
-router.get('/:id', albumController.returnedAlbums);
+router.get('/', albumController.returnedAlbums);
 
 router.get('/:id', albumController.albumById);
 
